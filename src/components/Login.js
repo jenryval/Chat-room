006652d173c021a588b50352121e50d426b83b46
jenry valdes <jenryval93@gmail.com>
2021-07-13 21:50:53 -0500

    added the logg in feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { GoogleOutLined, FacebookOutLined } from '@ant-desing/icons'
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
 
 
 const Login = () => {
 return (
-    <div id='login-page'>
+    <div id="login-page">
+        <div id="login-card">
+            <h2>Welcome to Unichat</h2>
 
+            <div
+            className="login-button google"
+            
+            >
+                <GoogleOutlined/> Sign In With Google
+            </div>
+    <br/> <br/>
+    <div
+            className="login-button facebook"
+            
+            >
+                <FacebookOutlined/> Sign In With Facebook   
+            </div>
+
+        </div>
     </div>
 )
 
