15f268b59f0383e6b3542bc5b03fda2eba6d98f2
jenry valdes <jenryval93@gmail.com>
2021-07-13 21:39:33 -0500

    added a new component

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { GoogleOutLined, FacebookOutLined } from '@ant-desing/icons'
+
+
+const Login = () => {
+return (
+    <div id='login-page'>
+
+    </div>
+)
+
+}
+
+export default Login; 
